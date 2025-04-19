d0568866cb8292525d8d268acd0020adcf32a042
tyrotoxism1 <jomomojo55@gmail.com>
2025-04-19 06:55:52 -0700

    added pulse module status, fixed function names and garbage first value of buf

--- src/main.c
+++ src/main.c
@@ -10,9 +10,12 @@ int main(void)
     UART_config();
     // 1 = .1ms of resolution, 15 = 15ms for timeout of transmission burst 
     pulse_measure_init(1,15);
+
     while(1){
-	    GPIOA->ODR |= GPIO_ODR_OD4;
-	    printf("testing\n");
+		if(pulse_measure_get_status() == PM_COMPLETE){
+			pulse_measure_print_values(1);
+			pulse_measure_set_status(PM_IDLE);
+		}
 
     }
     return 0;
